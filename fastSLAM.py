7b296e254d4d726cb7bc3e39e4e06b55ba94f2c9
hengl <henglin.h@gmail.com>
2017-10-19 16:55:00 +0200

    run w/o err, not sure about correctness

--- fastSLAM.py
+++ fastSLAM.py
@@ -9,12 +9,12 @@ class FastSlamParticle:
         self.pos_curr = np.zeros(shape=[3])
 
         self.covR = np.diag(np.ones(shape=3))    # dummy covariance
-        self.covQ = np.diag(np.ones(shape=3))    # dummy covariance
+        self.covQ = np.diag(np.ones(shape=2))    # dummy covariance
 
         # TODO: Check if the observed_feature carry on over different time step??
         self.observed_feature = list()  # store the id of landmark that is observed
-        self.features_prev = list()      # list of tuple? (id, mu, sigma)
-        self.features_curr = list()      # list of tuple? (id, mu, sigma)
+        self.features_prev = dict()      # dict of dict {id: {mu: 123, sigma:1}}
+        self.features_curr = dict()
 
         # TODO: Not sure if w is for single particle or 1 w for every feature?
         self.w = 0
@@ -40,20 +40,31 @@ class FastSlamParticle:
         self.pos_curr = np.random.multivariate_normal(np.squeeze(mean), self.covR)
 
     def update(self, measurements):    # measurement = [(id, dist, bearing), (id, dist, bearing)]
+        print 'updating...'
 
-        _w = list()
+        num_lm = measurements.shape[1]      # number of landmarks
 
-        for measurement in measurements:
+        _w = list()     # importance weight for the particle, (mean of weight calculated for all the features)
 
-            id = measurement[0]  # find the id of the feature
+        # iterate through all the measurement
+        for i in range(num_lm):
+            measurement = measurements[:, i]
+
+            # skip the features not being measured
+            if measurement[0] == 0 and measurement[1] == 0:
+                continue
+
+            id = i
 
             if id in self.observed_feature:
+                print 'observed feature'
 
-                mean_prev = self.features_prev[id]['mu']
-                sigma_prev = self.features_prev[id]['sigma']
+                mean_prev = self.features_prev[str(id)]['mu']
+                sigma_prev = self.features_prev[str(id)]['sigma']
 
-                # TODO: Get z_t from the measurement??
                 z_t = np.array([measurement[0], measurement[1]])
+
+                # TODO: Need to update self.pos_curr with z_t???
 
                 delta = mean_prev - self.pos_curr[:1]
                 q = np.dot(delta.T, delta)
@@ -62,34 +73,37 @@ class FastSlamParticle:
                 z_hat = np.array([np.sqrt(q), np.arctan2(delta[1], delta[0]) - self.pos_curr[2]]).T
 
                 # Jacobian of H with respect to location, STILL HAVE TO MULTIPLE WITH F_BIG
+                # 2x2
                 H = (1 / q) * np.array([
-                    [-np.sqrt(q) * delta[0], -np.sqrt(q) * delta[1], 0, np.sqrt(q) * delta[0], np.sqrt(q) * delta[1]],
-                    [delta[1], -delta[0], -q, -delta[1], delta[0]]
+                    [-np.sqrt(q) * delta[0], -np.sqrt(q) * delta[1]],
+                    [delta[1], -delta[0]]
                 ])
 
-                # measurement covariance
+                # measurement covariance, 2x2
                 covQ = np.dot(np.dot(H, sigma_prev), H.T) + self.covQ
 
-                # kalman gain
+                # kalman gain, 2x2
                 K = np.dot(np.dot(sigma_prev, H.T), np.linalg.inv(covQ))
 
-                # update mean
-                mean = mean_prev + K * (z_t - z_hat)
+                # update mean, 2x2
+                mean = mean_prev + np.dot(K, (z_t - z_hat))
 
-                # update covariance
-                sigma = (np.eye(3) - K * H) * sigma_prev
+                # update covariance, 2x2
+                sigma = np.dot((np.eye(2) - np.dot(K, H)), sigma_prev)
 
                 # importance factor
                 w = multivariate_normal(mean, sigma).pdf(z_t)
 
-                f = {'mu': mean,
-                     'sigma': sigma,
-                     'w': w
+                f = {
+                     'mu': mean,
+                     'sigma': sigma
                      }
-                self.features_curr.append(f)
+
+                self.features_curr[str(id)] = f
                 _w.append(w)        # record the importance weight of every landmark in every measurement?
 
             else:   # for those not observed
+                print 'not observed feature'
 
                 rx = self.pos_curr[0]
                 ry = self.pos_curr[1]
@@ -97,41 +111,47 @@ class FastSlamParticle:
 
                 # landmark location
                 mean = np.array([
-                    [rx + measurement[1] * np.cos(rtheta + measurement[2])],
-                    [ry + measurement[1] * np.sin(rtheta + measurement[2])]
-                ])
+                    rx + measurement[0] * np.cos(rtheta + measurement[1]),
+                    ry + measurement[0] * np.sin(rtheta + measurement[1])
+                ]).T
 
                 # difference between robot and landmark, delta[0] is x axis, delta[1] is y axis
-                delta = mean - self.pos_curr[:1]
+                delta = mean - self.pos_curr[:2]
 
                 q = np.dot(delta.T, delta)
 
-                # Jacobian of H with respect to location, STILL HAVE TO MULTIPLE WITH F_BIG
+                # 2x2
                 H = (1 / q) * np.array([
-                    [-np.sqrt(q) * delta[0], -np.sqrt(q) * delta[1], 0, np.sqrt(q) * delta[0], np.sqrt(q) * delta[1]],
-                    [delta[1], -delta[0], -q, -delta[1], delta[0]]
+                    [-np.sqrt(q) * delta[0], -np.sqrt(q) * delta[1]],
+                    [delta[1], -delta[0]]
                 ])
 
+                # 2x2
                 H_inv = np.linalg.inv(H)
+
+                # 2x2
                 sigma = np.dot(np.dot(H_inv, self.covQ), H_inv.T)
 
                 # default importance weight
-                w = 1 / len(measurements)
+                w = 1.0 / measurements.shape[1]
 
-                f = {'mu': mean,
-                     'sigma': sigma,
-                     'w': w
+                f = {
+                     'mu': mean,
+                     'sigma': sigma
                      }
 
                 self.observed_feature.append(id)
-                self.features_curr.append(f)
+                self.features_curr[str(id)] = f
                 _w.append(w)
 
-        for feature in self.features_prev:
-            if feature['id'] not in self.observed_feature:  # not sure about this line
-                self.features_curr.append(self.features_prev[feature['id']])
+        for feature_id, val in self.features_prev.iteritems():
+            if feature_id not in self.observed_feature:  # not sure about this line
+                self.features_curr[str(feature_id)] = val
 
+        # copy the calculated feature, pos to '*_prev', so '*_curr' can be override in next iteration
         self.pos_prev = self.pos_curr
+        self.features_prev = self.features_curr
+
         self.w = np.array(_w).mean()            # find the importance weight of this particle
         # reset the self.observed_feature??
 
@@ -144,7 +164,7 @@ def resample(w):
     result_ind = np.zeros(shape=[M])
 
     for m in range(M):
-        ind_m = np.where(cumsum_w > np.random.rand(1))[0]
+        ind_m = np.where(cumsum_w > np.random.rand(1))
         print ind_m
         if len(ind_m) != 0:
             result_ind[m] = ind_m[0]
--- main.py
+++ main.py
@@ -9,33 +9,53 @@ from utils import *
 
 from fastSLAM import FastSlamParticle
 from fastSLAM import resample
+from read_data import read_data
 
 
-time = 10
 num_particles = 1000
 
+# load the data
+u, x, z = read_data()
+
+time = u.shape[1]
+
+# experiment with 1 particle #####
+p1 = FastSlamParticle()
+p1.sample_pose(u[:, 0])
+p1.update(z[:, :, 0])
+# print '##### Updated... back to main function ... #####'
+# print 'importance weight: {}'.format(p1.get_importance_weight())
+
+p1.update(z[:, :, 0])
+# print '##### Updated... back to main function ... #####'
+# print 'importance weight: {}'.format(p1.get_importance_weight())
+
+
+
 # initialize a set of particles
-particle_set = [FastSlamParticle() for _ in range(num_particles)]
+# particle_set = [FastSlamParticle() for _ in range(num_particles)]
 
-for t in range(time):
+# for t in range(time):
+#     print 'time: {}'.format(t)
+#
+#     # data at the current time step
+#     u_t = u[:, t]
+#     x_t = x[:, t]
+#     z_t = z[:, :, t]
+#
+#     w = list()
+#
+#     for i, particle in enumerate(particle_set):
+#         particle.sample_pose(u_t)
+        # particle.update(z_t)
 
-    # TODO: Input action, measurement here
-    action = [0, 0]
-    measurement = []
+        # w_ = particle.get_importance_weight()
+        # w.append(w_)
 
-    w = list()
-
-    for particle in particle_set:
-        particle.sample_pose(action)
-        particle.update(measurement)
-
-        w_ = particle.get_importance_weight()
-        w.append(w_)
-
-    resampled_ind = resample(w)
-
-    # select the particle of those indices from the set
-    resampled_particle = [particle_set[ind] for ind in resampled_ind]
-
-    # set of particle for next time step
-    particle_set = resampled_particle
+    # resampled_ind = resample(w)
+    #
+    # # select the particle of those indices from the set
+    # resampled_particle = [particle_set[int(ind)] for ind in resampled_ind]
+    #
+    # # set of particle for next time step
+    # particle_set = resampled_particle
