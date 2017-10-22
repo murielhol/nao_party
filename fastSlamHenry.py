0bbc63a07d411ba9190e9e375198f8761ba619f6
hengl <henglin.h@gmail.com>
2017-10-22 21:08:04 +0200

    the ugliest code ive ever written, thats why i dont like names on file ;)

--- fastSlamHenry.py
+++ fastSlamHenry.py
@@ -6,10 +6,14 @@ import matplotlib.pyplot as plt
 from read_data import read_data
 
 
-def resample(w):
-    w = np.array(w)
-    # w = w / np.sum(w)
+def normal_density(x, mu, sigma):
+    return np.exp(-.5 * np.dot(np.dot((x - mu).T, np.linalg.inv(sigma)), (x - mu)))/np.sqrt(2*np.pi*np.linalg.det(sigma))
 
+
+def resample(w):
+
+    w = np.array(w)
+    w = w / np.sum(w)
     M = w.shape[0]
     cumsum_w = np.cumsum(w)
 
@@ -30,33 +34,23 @@ class Particle:
     def __init__(self, debug=False):
         self.debug = debug
         self.pose = np.zeros(shape=[3])
-        self.features = dict()
         self.rot1 = 0
 
         self.features_prev = dict()
         self.features_curr = dict()
 
-        self.covR = 0.0001 * np.eye(3)
-        self.covQ = 0.0001 * np.eye(2)
+        self.covR = 0.00001 * np.eye(3)
+        self.covQ = 0.1 * np.eye(2)
 
         self.w = 0
 
     def sample_pose(self, u):
-        v = u[0]
-        theta = u[1]
 
         # predicted robot position mean
-        x_new = np.array([
-            self.pose[0] + v * np.cos(self.pose[2] + self.rot1),
-            self.pose[1] + v * np.sin(self.pose[2] + self.rot1),
-            self.pose[2] + theta
-        ])
+        x_new = find_next_pos(self.pose, u, self.rot1, self.covR)
 
-        if self.debug:
-            print 'Estimated pose: {}'.format(x_new)
-            print 'Original pose: {}'.format(self.pose)
-
-        self.pose = np.random.multivariate_normal(np.squeeze(x_new), self.covR)
+        self.pose = x_new
+        # self.rot1 = self.pose[2].copy()
 
         if self.debug:
             print 'Sampled pose: {}'.format(self.pose)
@@ -67,12 +61,13 @@ class Particle:
         for i in range(6):      # iterate through 6 landmarks
             z_t = measurement[:, i]         # measurement for 1 landmark
             featureID = str(i)
+            w = 1
 
             if z_t[0] == 0 and z_t[1] == 0:     # if no observation
                 # if a feature is observed before, and not being observed now
                 # leave that feature unchanged.
                 if featureID in self.features_prev:
-                    self.features_curr[featureID] = self.features_prev[featureID]
+                    self.features_curr[featureID] = self.features_prev[featureID].copy()
                 continue
 
             if featureID in self.features_prev:     # observed feature
@@ -85,7 +80,7 @@ class Particle:
                 q = np.dot(delta.T, delta)
 
                 # measurement prediction
-                z_hat = np.array([np.sqrt(q), np.arctan2(delta[1], delta[0])]).T
+                z_hat = np.array([np.sqrt(q), np.arctan2(delta[1], delta[0]) + self.pose[2]]).T
 
                 # Jacobian of H with respect to location
                 # 2x2
@@ -111,8 +106,10 @@ class Particle:
                     # update covariance, 2x2
                     sigma = np.dot((np.eye(2) - np.dot(K, H)), sigma)
 
-                # importance factor
-                w = multivariate_normal(z_hat, Q).pdf(z_t)
+                    # importance factor
+                    w = normal_density(inno, np.zeros(2), Q)
+                else:
+                    w = 0.0000000001
 
                 self.features_curr[featureID]['mu'] = mean
                 self.features_curr[featureID]['sigma'] = sigma
@@ -141,7 +138,8 @@ class Particle:
                 sigma = np.dot(np.dot(H_inv, self.covQ), H_inv.T)
 
                 # default importance weight
-                w = 0.00001
+                Q = np.dot(np.dot(H, sigma), H.T) + self.covQ
+                w = normal_density(0, np.zeros(2), Q)
 
                 f = {
                      'mu': mean,
@@ -151,64 +149,90 @@ class Particle:
                 self.features_curr[featureID] = f
                 _w.append(w)
 
-        self.features_prev = self.features_curr
+        self.features_prev = self.features_curr.copy()
         self.w = np.array(_w).sum()
+
+
+def find_next_pos(pos, u, rot1, sigma):
+
+    mean = np.array([
+        pos[0] + u[0] * np.cos(pos[2] + rot1),
+        pos[1] + u[0] * np.sin(pos[2] + rot1),
+        pos[2] + u[1]
+    ])
+    pos = np.random.multivariate_normal(np.squeeze(mean), sigma)
+
+    return pos
 
 
 u, x, z = read_data()
 print u.shape, x.shape, z.shape
 
+Lx = [-15, -15, 0, 0, 15, 15]
+Ly = [-10, 10, -10, 10, -10, 10]
+
 num_particles = 100
 
 particles = [Particle() for _ in range(num_particles)]
 
-for t in range(140, 301):
+
+plt.style.use('ggplot')
+plt.figure()
+for t in range(140, 701):
 
     w = list()
 
-    print '======= t = {} ======='.format(t)
+    true_pos = x[:, t]
 
     for i in range(num_particles):
         particles[i].sample_pose(u[:, t])
-        # particles[i].update(z[:, :, t])
+        particles[i].update(z[:, :, t])
 
-        # w.append(particles[i].w)
+        w.append(particles[i].w)
 
-    # resampled_ind = resample(w)
+    resampled_ind = resample(w)
 
     # select the particle of those indices from the set
-    # resampled_particle = [particles[int(ind)] for ind in resampled_ind]
+    resampled_particle = [particles[int(ind)] for ind in resampled_ind]
+
+    test = list()
+
+    for ind in resampled_ind:
+        tmp = Particle()
+        tmp.pose = particles[int(ind)].pose.copy()
+        tmp.features_curr = particles[int(ind)].features_curr.copy()
+        tmp.features_prev = particles[int(ind)].features_prev.copy()
+
+        test.append(tmp)
 
     # set of particle for next time step
-    # particles = resampled_particle
+    particles = test
 
     if t % 100 == 0:
-        xmean = [j.pose[0] for j in particles]
-        ymean = [j.pose[1] for j in particles]
+        print '======= t = {} ======='.format(t)
+        xmean, ymean = list(), list()
+        rot = list()
 
-        plt.figure()
-        plt.scatter(xmean, ymean)
-        plt.show()
+        lmx = [list() for _ in range(6)]
+        lmy = [list() for _ in range(6)]
 
-        if t == 300:
-            print '####################'
-            w = list()
-            for i in range(num_particles):
-                pose = particles[i].pose
-                print pose[:2]
-                w_tmp = multivariate_normal(pose[:2], np.array([[1, 0], [0, 1]])).pdf(np.array([2.5, -3.5]))
-                print w_tmp
-                # print w_tmp
-                w.append(w_tmp)
+        for particle in particles:
+            xmean.append(particle.pose[0])
+            ymean.append(particle.pose[1])
+            rot.append(particle.pose[2])
 
-            resampled_ind = resample(w)
-            resampled_particle = [particles[int(ind)] for ind in resampled_ind]
-            particles = resampled_particle
+            for idx in range(6):
+                if str(idx) in particle.features_curr:
+                    lmx[idx].append(particle.features_curr[str(idx)]['mu'][0])
+                    lmy[idx].append(particle.features_curr[str(idx)]['mu'][1])
 
-            xmean = [j.pose[0] for j in particles]
-            ymean = [j.pose[1] for j in particles]
+        for idx in range(6):
+            plt.plot(lmx[idx], lmy[idx], '.')
+        plt.plot(true_pos[0], true_pos[1], 'x')
+        plt.plot(Lx, Ly, 's')
+        plt.xlim()
 
-            plt.figure()
-            plt.scatter(xmean, ymean)
-            plt.show()
+        plt.scatter(xmean, ymean, alpha=0.5)
 
+plt.plot(Lx, Ly, 's', c='k')
+plt.show()
